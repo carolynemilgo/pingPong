3ce53abd72aa59fc7b3c0c339b6bfe43b25183c2
carolynemilgo <carolynemilgo@gmail.com>
2017-06-09 17:44:42 +0300

    Dried code and updated css

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
-    var num;
-    for (var i = 1; i <= num; i++) {
-      if (i % 15 === 0) {
-        console.log("Ping Pong");
+//Business logic
+function pingpong(num) {
+  var empty = []
+
+  for (var i = 1; i < num; i++) {
+    if (i % 15 === 0) {
+      empty.push('pingpong')
+    } else if
+      (i % 3 === 0) {
+        empty.push('Ping')
+      } else if (i % 5 === 0) {
+        empty.push('Pong')
+      } else {
+        empty.push(i)
       }
-      else if (i % 3 === 0) {
-        console.log("Ping");
-      }
-      else if (i % 5 === 0) {
-        console.log("Pong");
-      }
-      else{
-        console.log(num);
-      };
+    }
+    console.log(empty);
+    return empty;
+  }
+
+//User interface
+
+  $(document).ready(function() {
+    $("form#game").submit(function(event) {
+      event.preventDefault();
+          var num=$("input#inputNumber").val();
+
+      $("#results").text(" ");
+      var anotherNum = pingpong(num)
+
+       $("#results").append('<ul>')
+
+       anotherNum.forEach(function(newNum){
+          $('#results').append('<li>' + newNum + '</li>')
+       })
+       $("#results").append('<ul>')
+
+    });
+  });
